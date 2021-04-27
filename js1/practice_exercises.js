100e952170e1f80a8bc9f88314dc7be82023a9a0
Matthew Walker <matthew.r.walker4@gmail.com>
2021-04-26 22:50:52 -0500

    added more practice js

--- js1/practice_exercises.js
+++ js1/practice_exercises.js
@@ -22,3 +22,66 @@
 //         alert(`Do you ${userMadLib[1]} your ${userMadLib[2]} ${userMadLib[0]} ${userMadLib[3]}? That's hilarious!`);
 //     }
 // }
+
+
+var salesReport = {
+    title: 'Monthly Sales Report',
+    date: '03-17-2015',
+    office: 'Codeup',
+    employees: [
+        {
+            employeeNumber: '1',
+            name: {
+                first: 'Jane',
+                last: 'Janeway'
+            },
+            salesUnits: 3
+        },
+        {
+            employeeNumber: '2',
+            name: {
+                first: 'Tricia',
+                last: 'Triciason'
+            },
+            salesUnits: 5
+        },
+        {
+            employeeNumber: '3',
+            name: {
+                first: 'Jeannette',
+                last: 'Jeanson'
+            },
+            salesUnits: 4
+        },
+        {
+            employeeNumber: '4',
+            name: {
+                first: 'Charles Emmerson III',
+                last: 'Winchester'
+            },
+            salesUnits: 2
+        },
+        {
+            employeeNumber: '5',
+            name: {
+                first: 'Chet',
+                last: 'Chedderson'
+            },
+            salesUnits: 8
+        },
+    ],
+    getEmployeeCount: function() {
+       return salesReport.employees.length;
+    },
+    getTotalNumberOfSales: function() {
+        var totalSales = 0;
+        salesReport.employees.forEach(function(element) {
+            totalSales += element.salesUnits;
+        });
+        return totalSales;
+    },
+    getAverageSalesPerEmployee: function () {
+       var averageSalesPerEmployee = salesReport.getTotalNumberOfSales() / salesReport.getEmployeeCount();
+       return averageSalesPerEmployee;
+    }
+};
